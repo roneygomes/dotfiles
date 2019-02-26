cb07b38d54674d112487cbad3ca6f690a25e62fc
Roney Gomes <roney477@gmail.com>
2019-02-26 10:40:39 -0300

    adding z for jumping between directories

--- zsh/system.sh
+++ zsh/system.sh
@@ -4,3 +4,5 @@ alias ohmyzsh="vim ~/.oh-my-zsh"
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 
 FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+
+. $HOME/bin/z.sh
