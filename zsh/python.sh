0cef928642b99030358f50de4a538fe0e0467ff6
Roney Gomes <roney.gomes@thoughtworks.com>
2019-06-17 22:22:26 -0300

    A bunch of configs I left uncommitted.

--- zsh/python.sh
+++ zsh/python.sh
@@ -2,3 +2,10 @@ PATH=$PATH:~/Library/Python/3.7/bin
 
 alias python=python3
 alias pip=pip3
+
+export WORKON_HOME=~/.venv
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+
+mkdir -p ${WORKON_HOME}
+
+source /usr/local/bin/virtualenvwrapper.sh
