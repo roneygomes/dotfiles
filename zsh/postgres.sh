f15959d50bec317a59017e6bffc972e9be3599c6
Roney Gomes <roneygomes@users.noreply.github.com>
2019-10-15 13:26:47 -0300

    Add pgadmin Shorthand
    Simples function to start pgadmin from a container and open
    the resulting URL in the browser.

--- zsh/postgres.sh
+++ zsh/postgres.sh
@@ -1,1 +1,11 @@
 PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+
+function pgadmin {
+    docker run -p 9999:80 \
+        -e 'PGADMIN_DEFAULT_EMAIL=roney477@gmail.com' \
+        -e 'PGADMIN_DEFAULT_PASSWORD=1234' \
+        -v $HOME/.pgadmin:/var/lib/pgadmin \
+        -d dpage/pgadmin4;
+
+    open http://localhost:9999
+}
