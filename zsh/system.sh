0f4aef282f0c7f7906db3fc370c3607446508081
Roney Gomes <roneygomes@users.noreply.github.com>
2020-01-27 13:01:38 -0300

    Create UUID Function
    Generates a lower-cased uuid string that's print and sent to the
    clipboard.

--- zsh/system.sh
+++ zsh/system.sh
@@ -1,8 +1,13 @@
+function uuid() {
+  uuid_string=$(uuidgen | awk '{print tolower($0)}')
+  echo "$uuid_string" | pbcopy
+  echo "$uuid_string"
+}
+
 alias alert="osascript -e 'display notification \"Finished\" with title \"Alert\"'"
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
-alias uuid="uuidgen | pbcopy"
 
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
 export LC_ALL=en_US.UTF-8
