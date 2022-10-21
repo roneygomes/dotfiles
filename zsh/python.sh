85e72ee54ddaa428f11113680d8128d18a4ee88b
Roney Gomes <roney.gomes@ifood.com.br>
2022-10-21 18:27:56 -0300

    fix: check virtualenvwrapper existence before loading
    verifying if there's a virtualenvwrapper installation under the current
    python version before sourcing

--- zsh/python.sh
+++ zsh/python.sh
@@ -1,14 +1,24 @@
+# pyenv is used for managing multiple python versions in the system
+# it allows us to install and switch between different python versions when required
 export PYENV_ROOT=$HOME/.pyenv
+
 export PATH=$PYENV_ROOT/bin:$PATH
 export PATH=$PYENV_ROOT/shims:$PATH
 
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
-export WORKON_HOME=~/.venv
-
-mkdir -p ${WORKON_HOME}
 
 eval "$(pyenv init -)"
 
+# virtualenvwrapper is a tool used for managing python virtual environment, which is a mechanism for
+# isolating project dependencies. that said, we can have multiple python versions each containing
+# many virtual environments
+export WORKON_HOME=~/.venv
+mkdir -p ${WORKON_HOME}
+
 PYTHON_VERSION=$(cat "$HOME"/.pyenv/version)
-# shellcheck disable=SC1090
-source "$HOME"/.pyenv/versions/"$PYTHON_VERSION"/bin/virtualenvwrapper.sh
+VIRTUAL_ENV_WRAPPER="$HOME"/.pyenv/versions/"$PYTHON_VERSION"/bin/virtualenvwrapper.sh
+
+if [ -f "$VIRTUAL_ENV_WRAPPER" ]; then
+  # shellcheck disable=SC1090
+  source "$VIRTUAL_ENV_WRAPPER"
+fi
