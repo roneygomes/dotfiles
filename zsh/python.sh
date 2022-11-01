3d7fbe24c2319795017c1520db59635977100d43
Roney Gomes <roney.gomes@ifood.com.br>
2022-10-31 21:31:54 -0300

    adjusting pyenv settings once more

--- zsh/python.sh
+++ zsh/python.sh
@@ -2,20 +2,23 @@
 # it allows us to install and switch between different python versions when required
 export PYENV_ROOT=$HOME/.pyenv
 
-export PATH=$PYENV_ROOT/bin:$PATH
 export PATH=$PYENV_ROOT/shims:$PATH
-
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
 
 eval "$(pyenv init -)"
 
-# virtualenvwrapper is a tool used for managing python virtual environment, which is a mechanism for
+# virtualenvwrapper is a tool used for managing python virtual environments, which is a mechanism for
 # isolating project dependencies. that said, we can have multiple python versions each containing
 # many virtual environments
 export WORKON_HOME=~/.venv
 mkdir -p ${WORKON_HOME}
 
 PYTHON_VERSION=$(cat "$HOME"/.pyenv/version)
+
+if [ -n "$PYTHON_VERSION" ]; then
+  export PATH=$PYENV_ROOT/versions/$PYTHON_VERSION/bin:$PATH
+fi
+
 VIRTUAL_ENV_WRAPPER="$HOME"/.pyenv/versions/"$PYTHON_VERSION"/bin/virtualenvwrapper.sh
 
 if [ -f "$VIRTUAL_ENV_WRAPPER" ]; then
